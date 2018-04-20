ce92909f442d2ec9b09f10d71e1523345fe97716
Sofia Sousa <csofiamsousa@gmail.com>
2018-04-20 12:23:48 +0100

    tp25591 Refactor for resources

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,33 @@
 // External Dependencies
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { map } from 'lodash';
+import 'bootstrap/dist/css/bootstrap.css';
 
 // Internal Dependencies
-import ArticlesIndex from './articles';
-import CategoriesIndex from './categories';
+import ResourcesIndex from './resources';
 
 class App extends React.Component {
 	render() {
-		return (
-			<div>
-				<h1>Welcome to StoryPage</h1>
-				<nav><Link to="/pages">Check all pages</Link></nav>
+		const resources = [ 'types', 'articles', 'categories' ];
 
-				<h2>Fake Resources</h2>
-				<hr />
-				<ArticlesIndex />
-				<hr />
-				<CategoriesIndex />
+		return (
+			<div className="container">
+				<section>
+					<h1>Welcome to StoryPage POC</h1>
+					<p>This is a POC using <em>Gutenberg package</em> ( <code>npm install @frontkom/gutenberg</code> )</p>
+
+					<p className="text-center">
+						<Link className="btn btn-primary" to="/pages">Try it!</Link>
+					</p>
+				</section>
+
+				<section>	
+					<p>Some resources like <b>Types</b>, <b>Articles</b> and <b>Categories</b> are requiered so editor can work.</p>
+					<p>Here are the requests and the data structure of those resources:</p>
+				</section>
+
+				{ map( resources, resource => <ResourcesIndex key={ `resource-${ resource }` } type={ resource } /> ) }
 			</div>
 		);
 	}
---
+++ src/components/resources/index.js
@@ -0,0 +1,116 @@
+// External Dependencies
+import { map, isEmpty, startCase, keys } from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Internal Dependencies
+import * as Actions from '../../store/actions';
+
+class ResourcesIndex extends React.Component {
+	constructor( props ) {
+		super( props );
+		
+		this.state = {
+			fields: [],
+		};
+	}
+
+	componentDidMount() {
+		this.props.fetchResources();
+	}
+
+	componentWillReceiveProps( nextProps ) {
+		if ( this.props.resources !== nextProps.resources ) {
+			const { resources } = nextProps;
+
+			if ( ! isEmpty( resources ) ) {
+				const fields = keys( map( resources )[ 0 ] );
+				this.setState( { fields } );
+			}
+		}
+	}
+
+	renderHeaders() {
+		const { fields } = this.state;
+
+		let headers = <th>No { this.props.type } found</th>;
+
+		if ( ! isEmpty( fields ) ) {
+			headers = fields.map( field => <th key={ `header-${ field }` }><code>{ field }:</code></th> );
+		}
+
+		return <thead><tr>{ headers }</tr></thead>;
+	}
+
+	renderBody() {
+		const { fields } = this.state;
+
+		if ( ! isEmpty( fields ) ) {
+			const body = map( this.props.resources, item => {
+				return <tr key={ `${ this.props.type }-${ item.id }` }>{ fields.map( field => {
+					return <td key={ field }><pre>{ JSON.stringify( item[ field ], null, 2 ) }</pre></td>;
+				} ) }</tr>;
+			} );
+
+			return <tbody>{ body }</tbody>;
+		}	
+	}
+
+	renderRequests() {
+		const requests = {
+			types: [
+				// { path: '/wp/v2/types', description: 'Get all types' },
+				{ path: '/wp/v2/types[slug]', description: 'Get one type by slug' },
+			],
+		};
+
+		const reqs = requests[ this.props.type ];
+
+		if ( ! isEmpty( reqs ) ) {	
+			return ( <table className="table table-bordered">
+				<thead>
+					<tr>
+						<th>apiRequest</th>
+						<th>Description</th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						map( reqs, req => {
+							return ( <tr key={ `req-${ req.path }` }>
+								<td><code>{ req.path }</code></td>
+								<td>{ req.description }</td>
+							</tr> );
+						} )
+					}
+				</tbody>
+			</table> );
+		}
+	}
+
+	render() {
+		return (
+			<section key={ this.props.key }>			
+				<h3>{ startCase( this.props.type ) }</h3>
+
+				{ this.renderRequests() }				
+
+				<table className="table table-striped">
+					{ this.renderHeaders() }					
+					{ this.renderBody() }
+				</table>
+			</section>
+		);
+	}
+}
+
+function mapStateToProps( state, ownProps ) {
+	return { resources: state[ ownProps.type ] };
+}
+
+function mapDispacthToProps( dispatch, ownProps ) {
+	return bindActionCreators( { fetchResources: Actions[ `fetch${ startCase( ownProps.type ) }` ] }, dispatch );
+}
+
+export default connect( mapStateToProps, mapDispacthToProps )( ResourcesIndex );
