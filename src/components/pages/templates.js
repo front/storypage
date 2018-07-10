dddb33b6af0d0f08ea49c1405e6f9475b5fc613d
Sofia Sousa <csofiamsousa@gmail.com>
2018-07-10 15:23:37 +0100

    Comment default page template

--- src/components/pages/templates.js
+++ src/components/pages/templates.js
@@ -1,107 +1,107 @@
-export default function getTemplates( { type } ) {
-
+export default function getTemplates() {
+	return '';
 	// minerva frontpage
-	return [
-		// hero-section
-		[ 'minerva/article-primary', { 
-			title: 'Handel dreier seg om å vinne',
-			teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-			category: 'Kommentar',
-		}, [] ],
-		// body
-		[ 'storypage/section', { className: 'container' }, [
-			[ 'storypage/row', { colmuns: 2, widths: '8,4' }, [
-				[ 'minerva/article-secondary', {
-					layout: 'col8 column-start1',
-					title: 'Handel dreier seg om å vinne',
-					teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-					category: 'Kommentar',
-				} ],
-				[ 'minerva/article-secondary', {
-					layout: 'col4 column-start9',
-					title: 'Handel dreier seg om å vinne',
-					teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-					category: 'Kommentar',
-				} ],
-			] ],
-		] ],
-		[ 'storypage/section', { className: 'container' }, [
-			[ 'storypage/row', { colmuns: 2, widths: '4,8' }, [ 
-				[ 'core/paragraph', { layout: 'col4 column-start1', placeholder: 'Add content' } ],
-				[ 'minerva/article-tertiary', {
-					layout: 'col8 column-start5',
-					title: 'Handel dreier seg om å vinne',
-					teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-					category: 'Kommentar',
-					imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-				} ],				
-				[ 'minerva/article-tertiary', {
-					layout: 'col8 column-start5',
-					title: 'Handel dreier seg om å vinne',
-					teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-					category: 'Kommentar',
-					imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-				} ],				
-				[ 'minerva/article-tertiary', {
-					layout: 'col8 column-start5',
-					title: 'Handel dreier seg om å vinne',
-					teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-					category: 'Kommentar',
-					imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-				} ],				
-				[ 'minerva/article-tertiary', {
-					layout: 'col8 column-start5',
-					title: 'Handel dreier seg om å vinne',
-					teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-					category: 'Kommentar',
-					imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-				} ],
-			] ],
-		] ],
-		[ 'storypage/section', { className: 'container' }, [
-			[ 'storypage/row', { colmuns: 2, widths: '6,6' }, [
-				[ 'core/paragraph', { layout: 'col6 column-start1', placeholder: 'An ad' } ],
-				[ 'minerva/podcast-box', { layout: 'col6 column-start7', customBackgroundColor: '#BF5048', title: 'Podkast', subtitle: 'Hør siste episode25: Episode 25: Kristoffer Egeberg', teaser: 'Minervapodden snakker med redaktør i faktisk.no, Kristoffer Egeberg, og spør rett ut: Ville faktasjekkens flaggskip vært bedre om de ansatte noen fra den innvandringskritiske høyresiden? På veien til det spørsmålet er vi innom Terje Tvedt, proteinpulver, Dagsavisens relevans, betydningen av viral spredning og problemene med den upolitiske journalisten.' } ],
-			] ],
-		] ],
-		[ 'storypage/section', { maxWidth: '980px' }, [
-			[ 'minerva/article-tertiary', {
-				title: 'Handel dreier seg om å vinne',
-				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-				category: 'Kommentar',
-				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-			} ],
-			[ 'minerva/article-tertiary', {
-				title: 'Handel dreier seg om å vinne',
-				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-				category: 'Kommentar',
-				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-			} ],
-			[ 'minerva/article-tertiary', {
-				title: 'Handel dreier seg om å vinne',
-				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-				category: 'Kommentar',
-				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-			} ],
-			[ 'minerva/article-tertiary', {
-				title: 'Handel dreier seg om å vinne',
-				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-				category: 'Kommentar',
-				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-			} ],
-			[ 'minerva/article-tertiary', {
-				title: 'Handel dreier seg om å vinne',
-				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-				category: 'Kommentar',
-				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-			} ],
-			[ 'minerva/article-tertiary', {
-				title: 'Handel dreier seg om å vinne',
-				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
-				category: 'Kommentar',
-				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
-			} ],
-			[ 'core/button', { className: 'minerva-button', align: 'center', text: 'Last flere artikler', customBackgroundColor: '#FFF', customTextColor: '#000' } ]
-		] ],
-	];
+	// return [
+	// 	// hero-section
+	// 	[ 'minerva/article-primary', { 
+	// 		title: 'Handel dreier seg om å vinne',
+	// 		teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 		category: 'Kommentar',
+	// 	}, [] ],
+	// 	// body
+	// 	[ 'storypage/section', { className: 'container' }, [
+	// 		[ 'storypage/row', { colmuns: 2, widths: '8,4' }, [
+	// 			[ 'minerva/article-secondary', {
+	// 				layout: 'col8 column-start1',
+	// 				title: 'Handel dreier seg om å vinne',
+	// 				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 				category: 'Kommentar',
+	// 			} ],
+	// 			[ 'minerva/article-secondary', {
+	// 				layout: 'col4 column-start9',
+	// 				title: 'Handel dreier seg om å vinne',
+	// 				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 				category: 'Kommentar',
+	// 			} ],
+	// 		] ],
+	// 	] ],
+	// 	[ 'storypage/section', { className: 'container' }, [
+	// 		[ 'storypage/row', { colmuns: 2, widths: '4,8' }, [ 
+	// 			[ 'core/paragraph', { layout: 'col4 column-start1', placeholder: 'Add content' } ],
+	// 			[ 'minerva/article-tertiary', {
+	// 				layout: 'col8 column-start5',
+	// 				title: 'Handel dreier seg om å vinne',
+	// 				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 				category: 'Kommentar',
+	// 				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 			} ],				
+	// 			[ 'minerva/article-tertiary', {
+	// 				layout: 'col8 column-start5',
+	// 				title: 'Handel dreier seg om å vinne',
+	// 				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 				category: 'Kommentar',
+	// 				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 			} ],				
+	// 			[ 'minerva/article-tertiary', {
+	// 				layout: 'col8 column-start5',
+	// 				title: 'Handel dreier seg om å vinne',
+	// 				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 				category: 'Kommentar',
+	// 				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 			} ],				
+	// 			[ 'minerva/article-tertiary', {
+	// 				layout: 'col8 column-start5',
+	// 				title: 'Handel dreier seg om å vinne',
+	// 				teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 				category: 'Kommentar',
+	// 				imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 			} ],
+	// 		] ],
+	// 	] ],
+	// 	[ 'storypage/section', { className: 'container' }, [
+	// 		[ 'storypage/row', { colmuns: 2, widths: '6,6' }, [
+	// 			[ 'core/paragraph', { layout: 'col6 column-start1', placeholder: 'An ad' } ],
+	// 			[ 'minerva/podcast-box', { layout: 'col6 column-start7', customBackgroundColor: '#BF5048', title: 'Podkast', subtitle: 'Hør siste episode25: Episode 25: Kristoffer Egeberg', teaser: 'Minervapodden snakker med redaktør i faktisk.no, Kristoffer Egeberg, og spør rett ut: Ville faktasjekkens flaggskip vært bedre om de ansatte noen fra den innvandringskritiske høyresiden? På veien til det spørsmålet er vi innom Terje Tvedt, proteinpulver, Dagsavisens relevans, betydningen av viral spredning og problemene med den upolitiske journalisten.' } ],
+	// 		] ],
+	// 	] ],
+	// 	[ 'storypage/section', { maxWidth: '980px' }, [
+	// 		[ 'minerva/article-tertiary', {
+	// 			title: 'Handel dreier seg om å vinne',
+	// 			teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 			category: 'Kommentar',
+	// 			imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 		} ],
+	// 		[ 'minerva/article-tertiary', {
+	// 			title: 'Handel dreier seg om å vinne',
+	// 			teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 			category: 'Kommentar',
+	// 			imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 		} ],
+	// 		[ 'minerva/article-tertiary', {
+	// 			title: 'Handel dreier seg om å vinne',
+	// 			teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 			category: 'Kommentar',
+	// 			imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 		} ],
+	// 		[ 'minerva/article-tertiary', {
+	// 			title: 'Handel dreier seg om å vinne',
+	// 			teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 			category: 'Kommentar',
+	// 			imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 		} ],
+	// 		[ 'minerva/article-tertiary', {
+	// 			title: 'Handel dreier seg om å vinne',
+	// 			teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 			category: 'Kommentar',
+	// 			imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 		} ],
+	// 		[ 'minerva/article-tertiary', {
+	// 			title: 'Handel dreier seg om å vinne',
+	// 			teaser: 'Trumps tiltagende handelskrig har sitt opphav i presidentens primitive syn på handel. Fredag truer en ny alvorlig omdreining.',
+	// 			category: 'Kommentar',
+	// 			imageUrl: 'https://www.minervanett.no/wp-content/uploads/2018/06/kunst_utstilling-960x500.jpg',
+	// 		} ],
+	// 		[ 'core/button', { className: 'minerva-button', align: 'center', text: 'Last flere artikler', customBackgroundColor: '#FFF', customTextColor: '#000' } ],
+	// 	] ],
+	// ];
 }
