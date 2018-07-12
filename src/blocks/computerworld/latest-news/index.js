84fe3da4fcc39d8f4cb95d7deab66df3187cda2b
Élio Cró <elio@front.no>
2018-07-12 18:06:47 +0100

    Renamed the previous latest news block

--- src/blocks/computerworld/latest-news/index.js
+++ src/blocks/computerworld/latest-news/index.js
@@ -2,26 +2,18 @@
 import React from 'react';
 import LatestNews from './LatestNews';
 
-import {
-  i18n,
-} from '@frontkom/gutenberg-js';
-
+import { i18n } from '@frontkom/gutenberg-js';
 const { __ } = i18n;
 
 
 // Block attriutes
-const attributes = {
-  image: {
-    type: 'string',
-    default: 'https://placeimg.com/600/280/people/grayscale',
-  },
-};
+const attributes = {};
 
 // Block name and settings
 export const name = 'computerword/latest-news';
 
 export const settings = {
-  title: __('CW Latest News'),
+  title: __('CW Latest News (React)'),
   description: __('Latest News from ComputerWorld'),
   icon: 'editor-table',
 
