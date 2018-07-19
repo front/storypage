f6b03af4939a51c07f4304ddb76ea14ffd48c4b4
Sofia Sousa <csofiamsousa@gmail.com>
2018-07-19 16:37:51 +0100

    refactor: Use an arrow function on input change event

--- src/components/form/input-n-submit.js
+++ src/components/form/input-n-submit.js
@@ -5,12 +5,6 @@ class InputNSubmit extends React.Component {
   constructor (props) {
     super(props);
     this.state = { inputValue: '' };
-
-    this.onChangeInput = this.onChangeInput.bind(this);
-  }
-
-  onChangeInput (event) {
-    this.setState({ inputValue: event.target.value });
   }
 
   render () {
@@ -23,23 +17,23 @@ class InputNSubmit extends React.Component {
 
     return (
       <div className="input-group mb-3">
-        <input 
-          type="text" 
-          className="form-control" 
-          placeholder="Recipient's username" 
-          aria-label="Recipient's username" 
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
           aria-describedby="basic-addon2"
           defaultValue={ value }
-          onChange={ this.onChangeInput }
+          onChange={ event => this.setState({ inputValue: event.target.value }) }
         />
         <div className="input-group-append">
-          <button 
-            className="btn btn-outline-success" 
+          <button
+            className="btn btn-outline-success"
             type="button"
             onClick={ () => onSubmit(inputValue) }
           >Ok</button>
-          <button 
-            className="btn btn-outline-danger" 
+          <button
+            className="btn btn-outline-danger"
             type="button"
             onClick={ onCancel }
           >Cancel</button>
