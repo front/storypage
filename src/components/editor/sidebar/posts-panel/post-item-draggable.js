d1bf7bda828f86054e05a0b3e0f4d1ef22c4d97d
Sofia Sousa <csofiamsousa@gmail.com>
2018-07-27 14:59:52 +0100

    fix: Remove unused code

--- src/components/editor/sidebar/posts-panel/post-item-draggable.js
+++ src/components/editor/sidebar/posts-panel/post-item-draggable.js
@@ -3,11 +3,10 @@
  */
 import React from 'react';
 import classnames from 'classnames';
-import { blocks, components, data, element } from '@frontkom/gutenberg-js';
+import { components, data, element } from '@frontkom/gutenberg-js';
 
 const { Draggable } = components;
 const { Component } = element;
-const { createBlock } = blocks;
 const { withSelect } = data;
 
 class PostItemDraggable extends Component {
@@ -22,30 +21,24 @@ class PostItemDraggable extends Component {
   componentDidMount () {
     const { post } = this.props;
 
-    // if(! this.state.block.uid) {
-      const block = {// createBlock(post.blockType, {
-        id: post.id,
-        title: [ post.title.rendered ],
-        link: post.link,
-        imageId: post.featured_media,
-        categoryId: post.categories[ 0 ],
-        authorId: post.author,
-        type: 'static',
-        layout: '',
-      };// );
+    const block = {
+      id: post.id,
+      title: [ post.title.rendered ],
+      link: post.link,
+      imageId: post.featured_media,
+      categoryId: post.categories[ 0 ],
+      authorId: post.author,
+      type: 'static',
+      layout: '',
+    };
 
-      this.setState({ block });
-    // }
+    this.setState({ block });
   }
 
   render () {
     const { isDragging, ...props } = this.props;
     const { index, rootUID } = this.props.insertionPoint;
     const { block } = this.state;
-
-    /* if (! block.uid) {
-      return '';
-    } */
 
     const className = classnames('components-posts-list-item-draggable', {
       'is-visible': isDragging,
