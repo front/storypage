c0baf74c08bc81d249f612db9f8bdd7f3e78f2d3
Élio Cró <elio@front.no>
2018-08-07 11:12:01 +0100

    tp26956 Added more attributes to the article block

--- src/blocks/computerworld/article/index.js
+++ src/blocks/computerworld/article/index.js
@@ -1,39 +1,52 @@
+/* eslint react/jsx-key: 0 */
+
 // External Dependencies
 import React from 'react';
 import {
   i18n,
   editor,
 } from '@frontkom/gutenberg-js';
+import ArticleSearch from './search';
 
 const { __ } = i18n;
 const {
   RichText,
+  // PlainText,
+  InspectorControls,
 } = editor;
 
 import './style.scss';
 
 // Block attriutes
-const attributes = {
+const attrs = {
+  id: {
+    type: 'number',
+    default: 0,
+  },
   title: {
-    type: 'array',
-    source: 'children',
+    type: 'string',
     selector: 'h2',
-    default: 'Wifi-nettverk på flyplasser sikkerhetsrisiko',
+    default: '',
   },
   teaser: {
-    type: 'array',
-    source: 'children',
+    type: 'string',
     selector: 'p',
-    default: 'I ytterste konsekvens kan det føre til at fremmede får kontroll over en flyplass og overtar flyplasstyringen, viser rapporten fra PA Consulting Group.',
+    default: '',
   },
   image: {
     type: 'string',
-    default: 'https://placeimg.com/600/280/people/grayscale',
+    default: '',
+  },
+  link: {
+    type: 'string',
+    default: '',
   },
 };
 
 // Block name and settings
 export const name = 'computerword/article';
+const imagePath = 'http://static.cw.newsfront.no/sites/default/files/styles/crop_image_main_large/public';
+const sitePath = 'http://www.cw.no';
 
 export const settings = {
   title: __('CW Article'),
@@ -41,45 +54,73 @@ export const settings = {
   icon: 'text',
 
   category: 'cw',
-  attributes,
+  attributes: attrs,
 
-  edit ({ attributes: attr, className, setAttributes }) {
-    const { title, teaser } = attr;
-    return (
+  edit ({ attributes, className, setAttributes }) {
+    const { id, title, teaser, image } = attributes;
+
+    // Helper to load the data from the article
+    function setArticle (article) {
+      console.log('Set article', article);
+      setAttributes({ id: article.entity_id });
+      setAttributes({ title: article.short_title });
+      setAttributes({ teaser: article.teaser });
+      setAttributes({ link: `${sitePath}/${article.path_alias}` });
+
+      const img = article.media && article.media.image && article.media.image.main;
+      if(img) {
+        setAttributes({ image: `${imagePath}/${img.path}` });
+      }
+    }
+
+    return [
+      <InspectorControls>
+        <hr />
+        <div>
+          <ArticleSearch select={ setArticle } />
+        </div>
+      </InspectorControls>,
       <div className={ className }>
-        <article className="cw-article">
-          <span>
-            <figure>
-              <img src="http://static.cw.newsfront.no/sites/default/files/styles/crop_image_landscape_large/public/dcx/flyplass_53723e.jpg" alt="" />
-            </figure>
-            <RichText
-              tagName="h2"
-              value={ title }
-              onChange={ value => setAttributes({ title: value }) }
-              inlineToolbar
-            />
-            <RichText
-              tagName="p"
-              className="teaser"
-              value={ teaser }
-              onChange={ value => setAttributes({ teaser: value }) }
-              inlineToolbar
-            />
-          </span>
-          <span className="readmore">les mer</span>
-        </article>
-      </div>
-    );
+        { !id ? (
+          <div className="components-placeholder">
+            <div className="components-placeholder__label">ComputerWorld Article</div>
+            <div className="components-placeholder__instructions">Please select an article from the sidebar.</div>
+          </div>
+        ) : (
+          <article className="cw-article">
+            <span>
+              <figure>
+                <img src={ image } alt="" />
+              </figure>
+              <RichText
+                tagName="h2"
+                value={ title }
+                onChange={ value => setAttributes({ title: value }) }
+                inlineToolbar
+              />
+              <RichText
+                tagName="p"
+                className="teaser"
+                value={ teaser }
+                onChange={ value => setAttributes({ teaser: value }) }
+                inlineToolbar
+              />
+            </span>
+            <span className="readmore">les mer</span>
+          </article>
+        ) }
+      </div>,
+    ];
   },
 
-  save ({ attributes: attr, className }) {
-    const { title, teaser } = attr;
+  save ({ attributes, className }) {
+    const { title, teaser, image, link } = attributes;
     return (
       <div className={ className }>
         <article className="cw-article">
-          <a href="http://www.google.com">
+          <a href={ link }>
             <figure>
-              <img src="http://static.cw.newsfront.no/sites/default/files/styles/crop_image_landscape_large/public/dcx/flyplass_53723e.jpg" alt="" />
+              <img src={ image } alt="" />
             </figure>
             <RichText.Content
               tagName="h2"
