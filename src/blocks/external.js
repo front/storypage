1c6c0634ff03c30058156c4fec163459fb0d8c42
Sofia Sousa <csofiamsousa@gmail.com>
2018-09-10 16:57:14 +0100

    Add g-content-in-columns external block

--- src/blocks/external.js
+++ src/blocks/external.js
@@ -5,3 +5,7 @@
 // Load external blocks
 import '@frontkom/g-hero-section';
 import '@frontkom/g-hero-section/build/style.css';
+
+// Load external blocks
+import '@frontkom/g-content-in-columns';
+import '@frontkom/g-content-in-columns/build/style.css';
